e4adb4e425993a49633b3c38e58f4e3ae4468b82
Isabel de Luis <ideluis@olin.edu>
2022-04-03 21:14:19 -0400

    Set up repo to turn in.

---
+++ sentiment_analysis.py
@@ -0,0 +1,212 @@
+# words that have an angry connotation
+ANGER = ["arguin'", "mood", "sociopath", "apathy", "wound","reused", \
+         "I hate to think that I was just your type", "shit", "deja vu", \
+         "cheat", "ain't playing", "pistol", "bleed", "liquor", "AK-40", \
+         "slaughter", "shooters", "don't", "mischievous", "fear", "fumbling"\
+         "screams", "killer", "wreckin'", "paranoid", "fuck off", "worst", \
+         "A-B-C-D-E,F you", "I'll never see again", "hate", "pistol", "glock",\
+         "stupid", "nasty", "screaming", "killed", "can't handle", "shoot", \
+         "police", "killing", "resentment", "hoes", "shootin'", "whores", \
+         "karma", "reeling", "jealousy"]
+
+# words that have a joy connotation
+JOY = ["lights", "energy", "music", "glitter", "life", "change", "holy", \
+       "good","rest", "best", "smile", "jumpin'", "young", "grow", "try", \
+       "bomb", "lit", "rock", "poppin'", "dancing", "happy", "surprise", \
+        "dancin'", "fame", "boss", "highs", "truth", "smiles", "smooth", "high"\
+        "sweep", "melt", "paradise", "beating", "louder", "sing", "dance", \
+        "fools", "golden", "champions", "top", "plaque", "Grammy", "flashy", \
+        "action", "diamond", "fame", "cash", "champagne-poppin'", "fancy", 
+        "bougie", "delight", "stuck", "easy", "hopes", "dancin'", "singin'", \
+        "happier", "christmas", "wish", "rockin'", "party", "sentimental", \
+        "jolly", "merrily", "rock", "hop", "chime", "swell", "jingle", "holly"\
+        "cheer", "wonderful", "happiest", "caroling", "parties", "feliz", \
+        "merry christmas", "prospero ano", "bottom of my heart", "navidad", \
+        "felicidad", "jingling", "lovely", "wonderland", "gift", "superstars", \
+        "peace", "pockets deep", "indestructible", "pushin' p", "life is good"\
+        "dreamin'","move", "again", "one more time", "never seen"\
+        "party", "fun", "gram", "attitude", "daddy's money", "pay up",\
+        "healing", "moved on", "walk away", "survive", "better", "asset", \
+        "money", "focused", "in the moment", "roll with", "say so", "slide",\
+        "passion", "satisfaction", "show you", "get it", "earned", "rockstar",\
+        "money", "mama", "good time", "i'm alive", "bitch", "bougie", "sassy"\
+        , "ratchet", "turn it up", "too fast", "chance", "swag", "draco", \
+        "pour", "millions", "summer feelin'", "taste", "sugar", "breathe", \
+        "wonderful", "fly", "rich", "stars", "light it up", "alight", "wet",\
+        "everything", "hard", "laugh", "whole", "juice", "drippin'", "risky"\
+        "fortune" "delightful", "lights are low", "hurry", "shooting star", \
+        "merry-go-round", "got to make it", "please", "popstar", "platinum", \
+        "fuckin' helicopter", "Drizzy's shit", "appreciate", "crown", \
+        "pedestal"]
+
+# words that have a love connotation
+LOVE = ["love", "touch", "trust", "cravin'", "show", "fuck", "ride", "shake"\
+        "baby", "want", "shining", "babe", "amazing", "real", "heaven", \
+        "positions", "infinite", "perfect", "saint", "altar", "union", \
+        "believer", "sex", "touch", "lift", "yours", "romancin'", "heart"\
+        "kissin'", "cuddlin'", "need", "like", "us", "cute", "romantic", 
+        "ridin'", "fuckin'", "hug", "cuddle", "lip", "hickeys", "sexy", \
+        "caress", "date", "warm", "sunlight", "needs", "heart", "kissed", \
+        "need", "fantasizin'", "sexual", "right", "you", "mistletoe", "special"\
+        "together", "snuggled", "porcelains", "lingers", "tempations", "love"\
+        "sex", "can't let go", "running", "let it go", "sweet heart", \
+        "love you", "ten thousand hours", "rest of my life", "love", \
+        "picture perfect", "intentions", "gorgeous", "walk through fire", \
+        "adore you", "love me", "only thing", "on my mind", "honey", \
+        "you want it", "committed", "feelin' myself", "treat you nice",\
+        "love me", "attached", "come with me", "shy", "you a freak", \
+        "ain't nobody watching", "shinin'", "pussy", "lover", "kiss me",\
+        "fallin'", "sparks", "love me so"]
+
+
+# words that have a sad connotation
+SADNESS = ["doubt", "insecure", "crying", "sorry", "gone", "blue", "depression"\
+           "victim", "empty", "drowning", "miss", "sinners", "bummed", "bear", \
+           "cry", "broke", "tears", "'lone", "hurtin", "gone", "dark", "pain"\
+           "traumatized", "tourture", "baggage", "deep", "numb", "freeze", \
+           "down", "burnt", "lost", "hurt", "was", "did", "lies", "betrayed"\
+           "traitor", "paranoid", "stay", "fucked", "nobody", "drunk", "wasted"\
+           "empty-handed", "alone", "last", "bad", "lose", "burned", "lost", \
+           "lonely", "drowning", "silence", "stuck", "remember", "gone", \
+           "grave", "lifeless", "cruel", "paralyzed", "fall", "terrified", \
+           "battle", "fight","fakin'", "fight", "hurt", "broken", "disbelief",\
+           "cold", "die","How could she do this to me?", "started off", "gave"\
+           "away", "fool", "breaks", "cold", "hardened", "seasons change",\
+            "memories", "drinks", "hurts", "raise a glass", "alright", \
+            "bring back you", "beg", "pain", "used to", "loved", "guard down",
+            "you're not here", "hurt", "lose", "hurt", "don't", "heartless", \
+            "low life", "pain", "back to my ways", "better man", "misery", \
+            "nothing", "dry", "bottled", "grieving", "dead", "discouraged",\
+            "ruin", "go", "drugs", "alone", "my fault", "miss me", "heavy"\
+            "cry", "ghost town", "half", "play", "break your heart", \
+            "you don't give two fucks", "wrecks", "cheats", "storm", \
+            "footprints", "erased", "pain", "refuse", "out", "remember", "hurt"\
+            , "hurting", "replace", "I needed to say", "kills", "tear me down",\
+            "chip on my shoulder", "unforgiveness", "break"]
+
+
+
+# Angsty words
+ANGST = ["cages", "could've", "switched", "watchin'", "worthless", "pressure", \
+         "flaw", "crushing", "human", "box", "soul", "drugs", "key", "mojo", \
+        "playin'", "cheapest", "lil bitch", "hunnid thousand", "turnt", "ego",
+        "monster", "normal", "mental hospital", "against the wall", \
+        "alcohol", "villain", "try", "blindly"]
+
+
+#need to finish, still thinking of a good way
+def mood_categorizer(word_list):
+    """
+    This function categorizes the overall mood of a list of words.
+
+    Arguments:
+        word_list: A list of words to be categorized
+    
+    Returns:
+        overall_mood: A string representing the overall mood of the list
+    """
+    overall_mood = ""
+
+    
+    for _ in word_list:
+        num_anger_words = 0
+        num_love_words = 0
+        num_joy_words = 0
+        num_sad_words = 0
+        num_angst_words = 0
+
+        for element in ANGER:
+            if element in word_list:
+                num_anger_words += 1
+
+        for element in JOY:
+            if element in word_list:
+                num_joy_words += 1
+
+        for element in LOVE:
+            if element in word_list:
+                num_love_words += 1
+        
+        for element in SADNESS:
+            if element in word_list:
+                num_sad_words += 1
+        
+        for element in ANGST:
+            if element in word_list:
+                num_angst_words += 1
+        
+        mood_values = [num_anger_words, num_sad_words, num_angst_words,\
+            num_joy_words, num_love_words]
+        
+        # Need to account for songs that have the equal number of certain types
+        # of words 
+
+        for element in mood_values:
+            if mood_values.count(element) > 1:
+                # take the mood of the first word in the list
+                if ANGER.count(word_list[0]) > 0:
+                    overall_mood = "anger"
+            
+                elif ANGST.count(word_list[0]) > 0:
+                    overall_mood = "angst"
+                
+                elif JOY.count(word_list[0]) > 0:
+                    overall_mood = "joy"
+                
+                elif LOVE.count(word_list[0]) > 0:
+                    overall_mood = "love"
+                else:
+                    overall_mood = "sad"
+                
+                return overall_mood
+
+
+        if max(mood_values) == num_anger_words:
+            overall_mood = "anger"
+        elif max(mood_values) == num_angst_words:
+            overall_mood = "angst"
+        elif max(mood_values) == num_joy_words:
+            overall_mood = "joy"
+        elif max(mood_values) == num_love_words:
+            overall_mood == "love"
+        elif max(mood_values) == num_sad_words:
+            overall_mood = "sad"
+
+        # Need to add code to save the overall_mood
+
+        return overall_mood
+
+def totals(month):
+    """
+    This function counts the total amount of each type of song for a given month
+
+    Arguments: 
+        - month: A dictionary of lists representing the key words in the top ten
+                 songs of the month
+
+    Returns:
+        - totals: A list representing the total number of each type of song 
+    """
+    anger_total = 0
+    love_total = 0
+    joy_total = 0
+    sad_total = 0
+    angst_total = 0
+    
+    for key in month:
+        list = month[key]
+    
+        if list[5::].count("sad") > 0:
+            sad_total += 1
+        elif list[5::].count("love") > 0:
+            love_total += 1
+        elif list[5::].count("joy") > 0:
+            joy_total += 1
+        elif list[5::].count("angst") > 0:
+            angst_total += 1
+        elif list[5::].count("anger") > 0:
+            anger_total += 1
+        
+
+
+    return [anger_total, love_total, joy_total, sad_total, angst_total]
